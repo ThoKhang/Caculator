bc625e6234848e2caaeefb63f37fe9b4c864f273
ThoKhang <khangheheqt@gmail.com>
2026-01-16 16:24:57 +0700

    add file;

---
+++ Caculator_ThoKhang580vn/src/main/java/bt/caculator_thokhang580vn/Caculator_ThoKhang580vn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bt.caculator_thokhang580vn;
+
+/**
+ *
+ * @author Admin
+ */
+public class Caculator_ThoKhang580vn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
